0bd0f2b2cb4d21a1cd3d2b689fdd1df0c7ea748b
Maikhaaa <MaikhaTheFirst@gmail.com>
2025-03-22 00:54:43 +0100

    what

--- lua/duckytype/window.lua
+++ lua/duckytype/window.lua
@@ -81,44 +81,16 @@ window_manager.create_window = function()
         	buffer = buffer,
         	callback = window_manager.update
     	})
-	--[[
+
 	if settings.centered then
 		local columns = (vim.api.nvim_get_option("columns") - settings.window_config.width) / 2
 		local rows = (vim.api.nvim_get_option("lines") - settings.window_config.height) / 2
 		settings.window_config.col = columns
 		settings.window_config.row = rows
 	end
-	]]
 
 	window = vim.api.nvim_open_win(buffer, true, settings.window_config)
 	vim.cmd('startinsert')
 end
-
-
---window.draw = function(vocabulary)
-
---      local done = true
---      --error(vim.api.nvim_buf_get_lines(buffer, 0, -1, false))
---      local lines = vim.api.nvim_buf_get_lines(buffer, 0, -1, false)
-
---      error(lines)
---      for index = 1, #vocabulary do
---      	local line = lines[index]
---      	local prefix = vocabulary[index]
-
---      	--local okay = methods.highlight_line(index - 1, line, prefix)
---      	--done = done and okay
-
---      	local cursor = vim.api.nvim_win_get_cursor(window.window)
---      	local row = cursor[1]
---      	if done and row == index then
---      		-- jump to next line if current line is okay
---      		vim.api.nvim_input('<Esc>jI')
---      		-- TODO (?)
---      		-- vim.api.nvim_win_set_cursor(window, { row + 1, 0 })
---      	end
---      end
---end
-
 
 return window_manager
